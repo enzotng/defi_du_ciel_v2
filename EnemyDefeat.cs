7512e59fc330b9365e04d4f93ff3c0b4793c150b
BigZoo92 <givernaudenzo@gmail.com>
2024-01-19 01:21:48 +0100

    zef

--- EnemyDefeat.cs
+++ EnemyDefeat.cs
@@ -3,11 +3,16 @@ using UnityEngine;
 public class EnemyDefeat : MonoBehaviour
 {
     public Material defeatedSkyboxMaterial; // Assignez cela dans l'inspecteur
+    public WallDestruction wallDestructionScript;
 
     public void OnEnemyDefeated()
     {
         // Changer la skybox
         RenderSettings.skybox = defeatedSkyboxMaterial;
+        if (wallDestructionScript != null)
+        {
+            wallDestructionScript.DestroyWall();
+        }
         // Vous pouvez également utiliser DynamicGI.UpdateEnvironment() si l'éclairage global doit être mis à jour
     }
 
