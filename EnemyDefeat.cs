293c463ccf944947884216899421d656a291f6aa
smoukeuur <enzotang@live.fr>
2024-01-19 01:43:02 +0100

    [fix] new scripts

--- EnemyDefeat.cs
+++ EnemyDefeat.cs
@@ -2,21 +2,18 @@ using UnityEngine;
 
 public class EnemyDefeat : MonoBehaviour
 {
-    public Material defeatedSkyboxMaterial; // Assignez cela dans l'inspecteur
+    public Material defeatedSkyboxMaterial;
     public WallDestruction wallDestructionScript;
 
     public void OnEnemyDefeated()
     {
-        // Changer la skybox
         RenderSettings.skybox = defeatedSkyboxMaterial;
         if (wallDestructionScript != null)
         {
             wallDestructionScript.DestroyWall();
         }
-        // Vous pouvez également utiliser DynamicGI.UpdateEnvironment() si l'éclairage global doit être mis à jour
     }
 
-    // Supposons que cette fonction est appelée lorsque l'ennemi est vaincu
     void DefeatEnemy()
     {
         OnEnemyDefeated();
